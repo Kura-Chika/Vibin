ff89adbe9d7294dfd6b2048257097a47cff45978
Kura-Chika <akkaihcchi@gmail.com>
2025-04-20 01:39:26 +0900

    Add public_group

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,13 +1,52 @@
 class Public::GroupsController < ApplicationController
-  def new
-  end
-
-  def index
+  
+  def index 
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new
+    # 誰が作ったグループかを判断するため
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :group_image)
+  end
+
+  # owner_idがログインuser_idと同じか確認。
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
+
 end
