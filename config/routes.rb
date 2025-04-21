e07c2a1c419a615b3d7f0399893520a636e825bd
Kura-Chika <akkaihcchi@gmail.com>
2025-04-21 21:31:54 +0000

    Fix error_routes2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   
   namespace :admin do
     root to: 'homes#top'
@@ -13,15 +21,15 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
     get 'homes/about' => 'homes#about', as: 'about'
-    resources :users, only: [:index, :show] do
-      collection do
-        get 'users/mypage' => 'users#mypage', as: 'mypage'
-        get 'users/information/edit' => 'users#edit', as: 'edit'
-        patch 'users/information' => 'users#update', as: 'update'
-        get  'users/unsubscribe' => 'users#unsubscribe', as:'unsubscribe' #退会確認画面へのパス
-        patch 'users/withdraw' => 'users#withdraw', as:'withdraw' #退会処理アクションのパス
-      end
-    end
+    
+    get 'users/mypage' => 'users#mypage', as: 'mypage'
+    get 'users/information/edit' => 'users#edit', as: 'edit'
+    patch 'users/information' => 'users#update', as: 'update'
+    get 'users/unsubscribe' => 'users#unsubscribe', as:'unsubscribe' #退会確認画面へのパス
+    patch 'users/withdraw' => 'users#withdraw', as:'withdraw' #退会処理アクションのパス
+    
+    resources :users, only: [:index, :show]
+
     resources :groups, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
       resource :group_users, only: [:create, :destroy]
     end
@@ -33,14 +41,6 @@ Rails.application.routes.draw do
     resources :artists, only: [:index, :show]
   end
   
-  devise_for :users, controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
-
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
 
   # 検索機能
   get 'search', to: 'searches#search'
